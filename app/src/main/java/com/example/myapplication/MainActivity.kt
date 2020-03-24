9d63ddaeccc8155d54a794fe71c2657fc6262f87
supriyaBharti15 <supriya.simi@gmail.com>
2020-03-24 21:41:00 +0530

    added a Button Dynamically using kotlin

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout.JUSTIFICATION_MODE_INTER_WORD
 import android.view.Gravity
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*
-        dynamic textView generated
 
-         */
         createTextViewDynamically()
+        createButtonDynamically()
 
         /*
         Button click and dynamically text added to the TextView...
@@ -28,11 +26,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /*
+        Create a new TextView programmatically in Kotlin Android
+
+     */
     private fun createTextViewDynamically() {
         val dynamicTextView = TextView(this)
-        dynamicTextView.text = "This is dynamically created textView this is my first kotlin example as i am learning any king of mistake ,can be taken as a part of learning making my self more error free.."
+        dynamicTextView.text =
+            "This is dynamically created textView this is my first kotlin example as i am learning any king of mistake ,can be taken as a part of learning making my self more error free.."
         dynamicTextView.textSize = 20f
         dynamicTextView.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
-        relLayout.addView(dynamicTextView)
+        mainLayout.addView(dynamicTextView)
+    }
+
+    /*
+    Create a new Button programmatically in Kotlin Android
+     */
+    private fun createButtonDynamically() {
+        val dynamicButton = Button(this)
+        dynamicButton.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        dynamicButton.text = "Dynamic Button"
+        dynamicButton.setBackgroundColor(Color.GREEN)
+        mainLayout.addView(dynamicButton)
     }
 }
